c2463ce396587e7e71b13a341857e1daaecd342a
Akito <kuroya1993@gmail.com>
2019-06-02 12:42:28 +0900

    controller作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,5 +5,11 @@ class MessagesController < ApplicationController
   end
 
   def create
+    Message.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:content, :image).merge{user_id: current_user.id}
   end
 end
