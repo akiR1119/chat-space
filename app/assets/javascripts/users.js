70f45f15502180a0156d045547474b10e8eb1cd8
Akito <kuroya1993@gmail.com>
2019-06-08 13:04:38 +0900

    HTML上でユーザーの追加と削除、既存メンバーを検索結果に表示しない機能

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,6 @@ $(function() {
   var preInput = "";
 
   function appendSearchedUser(user) {
-    // var delButton = user.id != current_user_id ?
     console.log("appending username")
     var userSearchResult = $("#user-search-result")
     var html = `<div class='chat-group-user clearfix' id="chat-group-user-${ user.id }">
@@ -12,13 +11,27 @@ $(function() {
     userSearchResult.append(html);
   }
 
-  function addUserToGroup(userId) {
-    
+  function addUserToGroup(userId, userName) {
+    //追加ボタンが押されたユーザーをチャットメンバーのところに追加する。
+    var userLists = $('#chat-group-users');
+    var html = `<div class='chat-group-user clearfix' id='chat-group-user-${userId}' data-user-id='${userId}'>
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <div class="chat-group-user__btn chat-group-user__btn--remove" data-user-id="${userId}">削除</div>
+                </div>`
+    userLists.append(html);
+  }
+
+  function removeUserFromGroup(userId) {
+    //削除ボタンが押されたユーザーをチャットメンバー欄から削除する。
+    console.log("do remove")
+    var userLists = $('#chat-group-users');
+    var removeUser = $(`#chat-group-user-${userId}`);
+    console.log($(`#chat-group-user-${userId}`))
+    console.log("削除対象=>" + removeUser)
+    removeUser.remove();
   }
 
   $('#user-search-field').on("keyup", function() {
-    // console.log($('#user-search-field').val());
-    console.log("hakka")
     //入力内容が変化している場合のみサーチを実行する。
     var input = $('#user-search-field').val();
     if (input !== preInput) {
@@ -34,22 +47,54 @@ $(function() {
         console.log("ajax done");
         if (users.length !== 0) {
           users.forEach(function(user) {
-            appendSearchedUser(user);
+            // console.log($("#current_user").get(0));
+            // console.log($("#current_user").attr('data-cuurentuser-id'));
+            // var currentUserId = $("#current_user").attr('data-cuurentuser-id');
+            // if(user.id != currentUserId) {appendSearchedUser(user)};
+            //すでにいるユーザーは表示しないようにする。メンバー欄に表示しているユーザーを配列で取得、eachで重複がないか確認する。
+            // console.log("重複チェック用")
+            // console.log($('#chat-group-users .chat-group-user'));
+            console.log("eachに入る")
+            var notDuplicate = true
+            $('#chat-group-users .chat-group-user').each(function(i) {
+              //この中で重複しないか確認する。重複がなければnotDuplicateにtrueを入れる
+              //重複したものが一つでもあればnotDuplicateにfalseを入れる。
+              // console.log(this);
+              var checkUserId = $(this).attr('data-user-id')
+              console.log(checkUserId)
+              console.log(user.id)
+              if(user.id == checkUserId){notDuplicate = false};
+              //user.idとthis内のdata-user-idを比較して不一致ならture、一致ならfalseを
+            })
+            console.log(notDuplicate);
+            if(notDuplicate){appendSearchedUser(user)};
           })
         }
       })
       .fail(function() {
         alert("ユーザーの追加に失敗しました");
       })
-      preInput = input
+      preInput = input;
     }
   })
 
-  $(document).on('click','.chat-group-user__btn', function() {
-    console.log("click~");
+  $(document).on('click','.chat-group-user__btn--add', function() {
+    console.log("click~add");
     console.log($(this).attr('data-user-id'));
-    var userIdToAdd = $(this).attr('id');
-    addUserToGroup(userIdToAdd);
+    var userIdToAdd = $(this).attr('data-user-id');
+    console.log($(this).attr('data-user-name'));
+    var userNameToAdd = $(this).attr('data-user-name');
+    addUserToGroup(userIdToAdd, userNameToAdd);
     //どのユーザーが押されたか？を把握し、次にそのユーザーをメンバーに追加する処理（paramsとhtml）。
+    $("#chat-group-user-" + userIdToAdd).remove();
   })
+
+  $(document).on('click','.chat-group-user__btn--remove', function() {
+    // console.log("removebutton!!");
+    // console.log($(this));
+    var userIdToRemove = $(this).attr('data-user-id');
+    // console.log($(`chat-group-user-${userIdToRemove}`));
+    removeUserFromGroup(userIdToRemove);
+  })
+
 })
